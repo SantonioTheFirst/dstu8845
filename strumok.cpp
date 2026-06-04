c0200a9597eb2a9958626dfdf92036c23d3ea073
SantonioTheFirst <44162170+SantonioTheFirst@users.noreply.github.com>
2026-06-04 09:22:39 +0300

    Update strumok.cpp

--- strumok.cpp
+++ strumok.cpp
@@ -1,9 +1,7 @@
 #include "strumok_optimized.h"
-#include <inttypes.h>
 #include <cstring>
-#include <cstdio>
-#include <sstream>
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 
@@ -11,7 +9,7 @@ dstu8845::dstu8845(const uint64_t *S, const uint64_t *r, const uint64_t *key, co
 {
     memcpy(this->S, S, 128);
     memcpy(this->r, r, 16);
-    memcpy(this->key, key, sizeof(key) / sizeof(uint64_t));
+    memcpy(this->key, key, sizeof(key) / sizeof(key[0]));
     memcpy(this->iv, iv, 32);
             
     static uint64_t outfrom_fsm, fsmtmp, tmp;
@@ -54,142 +52,20 @@ dstu8845 dstu8845::dstu8845_512(const uint64_t *key, const uint64_t *iv)
         S[15] = key[0];
         r[0] = 0;
         r[1] = 0;
-
         return dstu8845(S, r, key, iv);
 }
 
 void dstu8845::print()
 {
-    for(size_t i = 0; i < 16; i++)
+    for(uint8_t i = 0; i < 16; i++)
     {
-        printf("S_%zu 0x%016" PRIx64 "\n", i, this->S[i]);
+        cout << "S_" << (unsigned)i << ": 0x" << hex << uppercase << setfill('0') << setw(16) << this->S[i] << dec << nouppercase << "\n";
     }
-    printf("r_0 0x%016" PRIx64 "\n", this->r[0]);
-    printf("r_0 0x%016" PRIx64 "\n", this->r[1]);
+    cout << "r_0: 0x" << hex << uppercase << setfill('0') << setw(16) << this->r[0] << dec << nouppercase << "\n";
+    cout << "r_1: 0x" << hex << uppercase << setfill('0') << setw(16) << this->r[1] << dec << nouppercase << "\n";
 }
-      
-
-dstu8845::dstu8845_512_verbose(const uint64_t *key, const uint64_t *iv)
-{
-  this->z_0 = 0;
-  memcpy(this->iv, iv, 32);
-  memcpy(this->key, key, 64);
-  this->S[0] = this->key[7] ^ this->iv[0];
-  this->S[1] = this->key[6];
-  this->S[2] = this->key[5];
-  this->S[3] = this->key[4] ^ this->iv[1];
-  this->S[4] = this->key[3];
-  this->S[5] = this->key[2] ^ this->iv[2];
-  this->S[6] = this->key[1];
-  this->S[7] = ~this->key[0];
-  this->S[8] = this->key[4] ^ this->iv[3];
-  this->S[9] = ~this->key[6];
-  this->S[10] = this->key[5];
-  this->S[11] = ~this->key[7];
-  this->S[12] = this->key[3];
-  this->S[13] = this->key[2];
-  this->S[14] = ~this->key[1];
-  this->S[15] = this->key[0];
-  
-  this->r[0] = 0;
-  this->r[1] = 0;
-
-  //verbose
-  for(uint8_t i = 0; i < 16; i++)
-  {
-    cout << "S_" << (unsigned)i << ": ";
-    stringstream ss;
-    ss<< hex << this->S[i];
-    string res ( ss.str() );
-    cout << res << endl;
-  }
-  for(uint8_t i = 0; i < 2; i++)
-  {
-    stringstream ss;
-    ss<< hex << this->r[i];
-    string res ( ss.str() );
-    cout << "r_" << (unsigned)i << ": " << res << endl;
-  }
-  cout << endl;
-
-  static uint64_t outfrom_fsm, fsmtmp, tmp;
-  for(uint8_t i = 0; i < 32; ++i)
-  {
-    outfrom_fsm = (this->r[0] + this->S[15]) ^ this->r[1];
-    tmp = this->a_mul(this->S[0]) ^ this->S[13] ^ this->ainv_mul(this->S[11]) ^ outfrom_fsm;
-    fsmtmp = this->r[1] + this->S[13];
-    this->r[1] = this->T(this->r[0]);
-    this->r[0] = fsmtmp;
-    for(uint8_t j = 0; j < 15; ++j)
-    {
-      this->S[j] = this->S[j + 1];
-    }
-    this->S[15] = tmp;
-    this->z_0 |= ((((this->S[0] ^ this->r[1] ^ (this->r[0] + this->S[15]))) & 1) << i);
     
-    //verbose
-    for(uint8_t j = 0; j < 16; j++)
-    {
-      cout << "S_" << (unsigned)j << ": ";
-      stringstream ss;
-      ss<< hex << this->S[j];
-      string res ( ss.str() );
-      cout << res << endl;
-    }
-    for(uint8_t j = 0; j < 2; j++)
-    {
-      stringstream ss;
-      ss<< hex << this->r[j];
-      string res ( ss.str() );
-      cout << "r_" << (unsigned)j << ": " << res << endl;
-    }
-    cout << endl;
-  }
-}
-
-dstu8845::dstu8845_512(const uint64_t *key, const uint64_t *iv)
-{
-  this->z_0 = 0;
-  memcpy(this->iv, iv, 32);
-  memcpy(this->key, key, 64);
-  this->S[0] = this->key[7] ^ this->iv[0];
-  this->S[1] = this->key[6];
-  this->S[2] = this->key[5];
-  this->S[3] = this->key[4] ^ this->iv[1];
-  this->S[4] = this->key[3];
-  this->S[5] = this->key[2] ^ this->iv[2];
-  this->S[6] = this->key[1];
-  this->S[7] = ~this->key[0];
-  this->S[8] = this->key[4] ^ this->iv[3];
-  this->S[9] = ~this->key[6];
-  this->S[10] = this->key[5];
-  this->S[11] = ~this->key[7];
-  this->S[12] = this->key[3];
-  this->S[13] = this->key[2];
-  this->S[14] = ~this->key[1];
-  this->S[15] = this->key[0];
-  
-  this->r[0] = 0;
-  this->r[1] = 0;
-
-  static uint64_t outfrom_fsm, fsmtmp, tmp;
-  for(uint8_t i = 0; i < 32; ++i)
-  {
-    outfrom_fsm = (this->r[0] + this->S[15]) ^ this->r[1];
-    tmp = this->a_mul(this->S[0]) ^ this->S[13] ^ this->ainv_mul(this->S[11]) ^ outfrom_fsm;
-    fsmtmp = this->r[1] + this->S[13];
-    this->r[1] = this->T(this->r[0]);
-    this->r[0] = fsmtmp;
-    for(uint8_t j = 0; j < 15; ++j)
-    {
-      this->S[j] = this->S[j + 1];
-    }
-    this->S[15] = tmp;
-    this->z_0 |= ((((this->S[0] ^ this->r[1] ^ (this->r[0] + this->S[15]))) & 1) << i);
-  }
-}
-
-uint64_t inline dstu8845::next_stream()
+uint64_t dstu8845::next_stream()
 {
   uint64_t fsmtmp, tmp, out_stream;
 
