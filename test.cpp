127e83465433cc867edb49aaee092aedd0c92ef6
SantonioTheFirst <44162170+SantonioTheFirst@users.noreply.github.com>
2026-06-12 04:29:26 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,9 +5,14 @@
 int main()
 {
     using namespace std;
-    alignas(64) uint64_t key[8] = {
+    alignas(64) uint64_t key_512[8] = {
     0x0000000000000000, 0x0000000000000000,
     0x0000000000000000, 0x0000000000000000,
+    0x0000000000000000, 0x0000000000000000,
+    0x0000000000000000, 0x8000000000000000
+    };
+
+    alignas(64) uint64_t key_256[8] = {
     0x0000000000000000, 0x0000000000000000,
     0x0000000000000000, 0x8000000000000000
     };
@@ -15,19 +20,38 @@ int main()
 
     uint32_t z_0 = 0;
     dstu8845 cipher;
-    clock_t begin = clock();
+    // warmup
     for(uint64_t i = 0; i < 1000'000; i++)
     {
-        cipher.dstu8845_512_fast(key, iv);
+        cipher.dstu8845_256_fast(key_256, iv);
+        z_0 ^= cipher.z_0;
+    }
+
+    clock_t begin = clock();
+    for(uint64_t i = 0; i < 10'000'000; i++)
+    {
+        cipher.dstu8845_256_fast(key_256, iv);
         z_0 ^= cipher.z_0;
     }
     clock_t end = clock();
     cout << z_0 << "\n";
     
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    cout << "Init time: " << time_spent << "\n" << "Init per second: " << 1000'000 / time_spent << "\n";
+    cout << "Init time 256 bit: " << time_spent << "\n" << "Init per second: " << 10'000'000 / time_spent << "\n";
+   
+    begin = clock();
+    for(uint64_t i = 0; i < 10'000'000; i++)
+    {
+        cipher.dstu8845_512_fast(key_512, iv);
+        z_0 ^= cipher.z_0;
+    }
+    end = clock();
+    cout << z_0 << "\n";
+    
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    cout << "Init time 512 bit: " << time_spent << "\n" << "Init per second: " << 10'000'000 / time_spent << "\n";
 
-    cipher.dstu8845_512_fast(key, iv);
+    cipher.dstu8845_512_fast(key_512, iv);
     cipher.print();
 
     alignas(64) const uint8_t in[4096] = {0};
